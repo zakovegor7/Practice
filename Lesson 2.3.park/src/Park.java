29aad86e02c512025f9b203115046311e0002e82
Zakovryashin Egor <zakovegor7@gmail.com>
2026-07-10 18:40:28 +0700

    Add files via upload

---
+++ Lesson 2.3.park/src/Park.java
@@ -0,0 +1,20 @@
+public class Park {
+    public class Attraction {
+        String name;
+        String workingHours;
+        double price;
+
+        public Attraction(String name, String workingHours, double price) {
+            this.name = name;
+            this.workingHours = workingHours;
+            this.price = price;
+        }
+
+        public void show() {
+            System.out.println("Аттракцион: " + name);
+            System.out.println("Время работы: " + workingHours);
+            System.out.println("Стоимость: " + price + " руб.");
+            System.out.println("-------------------");
+        }
+    }
+}
