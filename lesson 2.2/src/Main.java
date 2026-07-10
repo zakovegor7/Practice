559bd892163790d9d718432a76842eb530f84698
Zakovryashin Egor <zakovegor7@gmail.com>
2026-07-10 21:36:51 +0700

    Add files via upload

---
+++ lesson 2.2/src/Main.java
@@ -0,0 +1,188 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Задание 1");
+        printThreeWords();
+
+        System.out.println("\nЗадание 2");
+        checkSumSign();
+
+        System.out.println("\nЗадание 3");
+        printColor();
+
+        System.out.println("\nЗадание 4");
+        compareNumbers();
+
+        System.out.println("\nЗадание 5");
+        System.out.println(SumInRange(11, 10));
+
+        System.out.println("\nЗадание 6");
+        positiveOrNegative(5);
+
+        System.out.println("\nЗадание 7");
+        System.out.println(negative(1));
+
+        System.out.println("\nЗадание 8");
+        printString("привет", 3);
+
+        System.out.println("\nЗадание 9");
+        System.out.println(leapYear(2021));
+
+        System.out.println("\nЗадание 10");
+        invertArray();
+
+        System.out.println("\nЗадание 11");
+        fillArray();
+
+        System.out.println("\nЗадание 12");
+        multiplyLessThanSix();
+
+        System.out.println("\nЗадание 13");
+        fillDiagonal();
+
+        System.out.println("\nЗадание 14");
+        int[] result = createArray(12, 9);
+        for (int i = 0; i < result.length; i++) {
+            System.out.print(result[i]);
+        }
+        System.out.println();
+    }
+
+    // Задание 1
+    public static void printThreeWords() {
+        System.out.println("Orange");
+        System.out.println("Banana");
+        System.out.println("Apple");
+    }
+
+    // Задание 2
+    public static void checkSumSign() {
+        int a = 10;
+        int b = -9;
+        int sum = a + b;
+        if (sum >= 0) {
+            System.out.println("Сумма положительная");
+        } else {
+            System.out.println("Сумма отрицательная");
+        }
+    }
+
+    // Задание 3
+    public static void printColor() {
+        int value = 22;
+        if (value <= 0) {
+            System.out.println("Красный");
+        } else if (value <= 100) {
+            System.out.println("Желтый");
+        } else {
+            System.out.println("Зеленый");
+        }
+    }
+
+    // Задание 4
+    public static void compareNumbers() {
+        int a = 12;
+        int b = 10;
+        if (a >= b) {
+            System.out.println("a >= b");
+        } else {
+            System.out.println("a < b");
+        }
+    }
+
+    // Задание 5
+    public static boolean SumInRange(int a, int b) {
+        int sum = a + b;
+        return sum >= 10 && sum <= 20;
+    }
+
+    // Задание 6
+    public static void positiveOrNegative(int number) {
+        if (number >= 0) {
+            System.out.println("Положительное");
+        } else {
+            System.out.println("Отрицательное");
+        }
+    }
+
+    // Задание 7
+    public static boolean negative(int number) {
+        return number < 0;
+    }
+
+    // Задание 8
+    public static void printString(String str, int count) {
+        for (int i = 0; i < count; i++) {
+            System.out.println(str);
+        }
+    }
+
+    // Задание 9
+    public static boolean leapYear(int year) {
+        if (year % 4 == 0) {
+            return true;
+        }
+        return false;
+    }
+
+    // Задание 10
+    public static void invertArray() {
+        int[] array = {1, 1, 0, 0, 1, 0, 1, 1, 0, 0};
+        for (int i = 0; i < array.length; i++) {
+            array[i] = (array[i] == 0) ? 1 : 0;
+        }
+        for (int i = 0; i < array.length; i++) {
+            System.out.print(array[i]);
+        }
+        System.out.println();
+    }
+
+    // Задание 11
+    public static void fillArray() {
+        int[] array = new int[100];
+        for (int i = 0; i < array.length; i++) {
+            array[i] = i + 1;
+        }
+        for (int i = 0; i < array.length; i++) {
+            System.out.print(array[i]);
+        }
+        System.out.println();
+    }
+
+    // Задание 12
+    public static void multiplyLessThanSix() {
+        int[] array = {1, 5, 3, 2, 11, 4, 5, 2, 4, 8, 9, 1};
+        for (int i = 0; i < array.length; i++) {
+            if (array[i] < 6) {
+                array[i] = array[i] * 2;
+            }
+        }
+        for (int i = 0; i < array.length; i++) {
+            System.out.print(array[i]);
+        }
+        System.out.println();
+    }
+
+    // Задание 13
+    public static void fillDiagonal() {
+        int size = 5;
+        int[][] array = new int[size][size];
+        for (int i = 0; i < size; i++) {
+            array[i][i] = 1;
+        }
+        for (int i = 0; i < size; i++) {
+            for (int j = 0; j < size; j++) {
+                System.out.print(array[i][j]);
+            }
+            System.out.println();
+        }
+    }
+
+    // Задание 14
+    public static int[] createArray(int len, int initialValue) {
+        int[] array = new int[len];
+        for (int i = 0; i < len; i++) {
+            array[i] = initialValue;
+        }
+        return array;
+    }
+}
