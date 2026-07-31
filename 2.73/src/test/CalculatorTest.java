874f183caee2448bb81dfbc40e6d7f7db045e6e2
Zakovryashin Egor <zakovegor7@gmail.com>
2026-07-31 23:32:33 +0700

    Add files via upload

---
+++ 2.73/src/test/CalculatorTest.java
@@ -0,0 +1,37 @@
+package test;
+
+import main.Calculator;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class CalculatorTest {
+
+    @Test
+    void additionShouldWorkCorrectly() {
+        assertEquals(8, Calculator.add(5, 3));
+    }
+
+    @Test
+    void subtractionShouldWorkCorrectly() {
+        assertEquals(2, Calculator.subtract(5, 3));
+    }
+
+    @Test
+    void multiplicationShouldWorkCorrectly() {
+        assertEquals(15, Calculator.multiply(5, 3));
+    }
+
+    @Test
+    void divisionShouldWorkCorrectly() {
+        assertEquals(2, Calculator.divide(6, 3));
+    }
+
+    @Test
+    void divisionByZeroShouldThrowException() {
+        assertThrows(
+                ArithmeticException.class,
+                () -> Calculator.divide(10, 0)
+        );
+    }
+}
