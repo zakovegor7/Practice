dd0f74821d708824498a0a77b5636085fe0cb234
Zakovryashin Egor <zakovegor7@gmail.com>
2026-08-07 23:48:09 +0700

    Delete 2.73 directory

--- 2.73/src/main/Triangle.java
+++
@@ -1,9 +0,0 @@
-package main;
-public class Triangle{
-    public static double calculate(double base, double height) {
-        if (base <= 0 || height <= 0) {
-            throw new IllegalArgumentException("Величины должны быть положительными");
-        }
-        return 0.5 * base * height;
-    }
-}
